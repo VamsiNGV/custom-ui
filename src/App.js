30d1fd499b6259fa62c7b3420e6e4b758dbcabca
Vamsi NGV <vamsi.nookala@gmail.com>
2024-01-22 10:00:06 +0530

    Commiting initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Modal from './components/Modal/Modal';
+
+const appData = [
+  {
+    id: "1",
+    name: "Coupa",
+    imageSource: "",
+    appLink: "https://www.youtube.com/embed/YJ5EMzyimfc?si=9U5mnId5gGQZMm1W",
+  },
+  {
+    id: "2",
+    name: "CMD",
+    imageSource: "",
+    appLink: "3",
+  },
+  {
+    id: "3",
+    name: "Secure",
+    imageSource: "",
+    appLink: "3",
+  },
+];
 
 function App() {
+
+  const [apps, setApps] = useState(appData);
+  const [showModal, setShowModal] = useState(false);
+  
+  
+  const openApp = (appName, appFrameSrc) => {
+    console.log(`You clicked on ${appName}`);
+    // setActiveAppName(appName);
+    // setAppFrameSrc(appFrameSrc);
+    setShowModal(true);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='layout__wrapper'>
+      <div className='card'>
+        <div className='header'>
+          <h1>UHG Apps</h1>
+        </div>
+        {apps.map(app => (
+          <div key={app.id}>
+            <div className='app-container'>
+              <button className='app-icon' onClick={() => openApp(app.name, app.appLink)}>
+                <h3>{app.name}</h3>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Modal Window */}
+      {showModal && (
+        <Modal />
+      )}
     </div>
   );
 }
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Backdrop from '../Backdrop/Backdrop'
+
+
+function Modal() {
+  return (
+    <>
+        <Backdrop />
+            <div className='modal'>
+                Modal
+            </div>
+    </>
+  )
+}
+
+export default Modal
